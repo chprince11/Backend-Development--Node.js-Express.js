956bddc0ff393e49eaff02a77e6c8cbbd960ee73
chprince11 <chprince@sheridancollege.ca>
2024-04-12 02:45:34 -0400

    changes-index.js

--- NPM/index.js
+++ NPM/index.js
@@ -1,17 +1,26 @@
 /* 
-npm ( Node Package Manager )
- npm comes pre-bundled with Node, so if have Node installed, you also have NPM installed.
+npm ( Node Package Manager ) 
+- npm is like community tool library. It is open-source. 
 
- Step-1: Initialize npm ( npm init ) - configuration file will download 
+- npm comes pre-bundled with Node, so if have Node installed, you also have NPM installed.
 
- Step-2: npm install 'Name of the packages' or npm i 'package name'
+ Step-1: Initialize npm ( npm init ) - -configuration file will download ( package.json ). 
+ - It basically brings up the initialization utility, which help us to create our config. file, which is called package.json.
 
+ Step-2: Have to install the npm package 
+ npm install 'Name of the packages' or npm i 'package name'
 
 */
-// CJS module
-// var generateName = require('sillyname');
+// ** CJS module **
 
-//ESM ECMAScript module
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+// console.log(`My name is ${sillyName}.`);
+
+// ** ESM ECMAScript module **
+
+// To use the ESM we have to go to the package.json and after the main comma have insert this key value pair ' "type": module, ', now you are good to use ESM 
+
 import generateName from "sillyname";
 var sillyName = generateName();
 
