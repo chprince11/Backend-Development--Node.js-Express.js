84be14a9bcbb2f77c8c0ec0f74f2bf153d59c2a6
chprince11 <chprince@sheridancollege.ca>
2024-04-12 02:31:23 -0400

    one-comment-added

--- Node/index.js
+++ Node/index.js
@@ -4,6 +4,8 @@
 // CJS (Comman JS pattern )
 const fs = require('node:fs');
 
+// Inserted a file system (fs) modulue , then we call the method writeFile which have 3 parameters to pass on 1) the file name 2) data into that file 3) the callback function for handing the error
+
 // fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
 //     if(err) throw err;
 //     console.log("The file has been saved!");
