41ae88716b4aa76ebf57bd3869551f7a743bd651
chprince11 <chprince@sheridancollege.ca>
2024-04-16 03:13:29 -0400

    Added-ExpressJS

---
+++ ExpressJS/intro.js
@@ -0,0 +1,10 @@
+/*
+Express Framework
+- A framework on top of Node
+- Express is a JS framework that allowed to create a backend for the websites. ( node.js + express.js )
+
+- Why to use Express ?
+- Readability
+- Less Code
+- Middleware
+*/ 
