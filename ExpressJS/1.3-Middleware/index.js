771703e509212ef8ca8ca224b0c0a7b60bb1c1ef
chprince11 <chprince@sheridancollege.ca>
2024-04-22 15:47:49 -0400

    Midleware Added

---
+++ ExpressJS/1.3-Middleware/index.js
@@ -0,0 +1,21 @@
+/* 
+
+// Postman 
+
+HTTP Response Status Codes:
+
+1) Information responses  ( 100 - 199 )
+2) Successful responses   ( 200 - 299 )
+3) Redirection messages   ( 300 - 399 )
+4) Client error responses ( 400 - 499 )
+5) Server error responses ( 500 - 599 )
+
+*/ 
+
+// Express Middleware -  It is something which sit in between the raw requests that come in.
+
+// They preprocess the request
+// Also help us to put the authentication between the server to the respected http requests handlers. 
+// process any sort of error
+
+// The commanly used middlware is Body Parser. Mainly used to handle the form data.
