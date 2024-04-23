5366f6e3a4b3defe24b9d4b2fee2ac795891348d
chprince11 <chprince@sheridancollege.ca>
2024-04-23 08:46:45 -0400

    Updated 1.3

--- ExpressJS/1.3-Middleware/index4.js
+++ ExpressJS/1.3-Middleware/index4.js
@@ -1,7 +1,31 @@
-import express from "express";
+import express, { response } from "express";
+import bodyParser from 'body-parser';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+var fullName = "";
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+function fullNameGenerator( req, res, next) {
+  console.log(req.body);
+  fullName = req.body['firstname'] + ' ' + req.body['lastname'];
+  next();
+}
+
+app.use(fullNameGenerator);
+
+app.get("/" , (request, response) => {
+  response.sendFile(__dirname + "/public/index.html");
+
+});
+
+app.post("/submit", (req,response) => {
+  response.send(`<h1> Your full name is: ${fullName}</h1>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
