d5afada9b6659d1a41704eaf37c8f5f0c5b95c9d
chprince11 <chprince@sheridancollege.ca>
2024-04-22 20:18:23 -0400

    Created own middleware

--- ExpressJS/1.3-Middleware/index3.js
+++ ExpressJS/1.3-Middleware/index3.js
@@ -3,6 +3,13 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+function logger(req,res,next){
+  console.log("Request method: ", req.method);
+  console.log("Request method: ", req.url);
+  next();
+};
+
 app.use(logger);
 
 app.get("/", (req, res) => {
