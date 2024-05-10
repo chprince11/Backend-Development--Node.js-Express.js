f1c45896907f13f801399a3243e61132fc1d9319
chprince11 <chprince@sheridancollege.ca>
2024-05-09 21:37:45 -0400

    Added 1.5-Ejs

--- ExpressJS/1.3-Middleware/index4.js
+++ ExpressJS/1.3-Middleware/index4.js
@@ -1,29 +1,30 @@
-import express, { response } from "express";
-import bodyParser from 'body-parser';
-import {dirname} from 'path';
-import {fileURLToPath} from 'url';
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
 var fullName = "";
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-function fullNameGenerator( req, res, next) {
+function fullNameGenerator(req, res, next) {
   console.log(req.body);
-  fullName = req.body['firstname'] + ' ' + req.body['lastname'];
+  fullName = req.body["firstname"] + " " + req.body["lastname"];
   next();
 }
 
 app.use(fullNameGenerator);
 
-app.get("/" , (request, response) => {
+app.get("/", (request, response) => {
   response.sendFile(__dirname + "/public/index.html");
-
 });
 
-app.post("/submit", (req,response) => {
+app.post("/submit", (req, response) => {
   response.send(`<h1> Your full name is: ${fullName}</h1>`);
 });
 
--- ExpressJS/1.5-Ejs/index.js
+++ ExpressJS/1.5-Ejs/index.js
@@ -1,28 +1,25 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
+  const today = new Date();
+  const day = today.getDay();
 
-    const today = new Date();
-    const day = today.getDay();
+  let type = "a weekday";
+  let adv = "it's time to work hard";
 
-    let type = "a weekday";
-    let adv = "it's time to work hard";
-
-    if(day === 0 || day === 6) {
-        type = "the weekend";
-        adv = "it's time to have some fun";
-    }
-    res.render('index.ejs', {
-        dayType: type, 
-        advice: adv,
-    });
-
+  if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "it's time to have some fun";
+  }
+  res.render("index.ejs", {
+    dayType: type,
+    advice: adv,
+  });
 });
 
-
-app.listen(port, () =>{
-    console.log(`Server running on port ${port}.`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
 });
